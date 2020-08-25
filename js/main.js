c249f6dd7901cafd8cf9b190ebf4474adcb1fcf2
Bakkar1 <68107222+Bakkar1@users.noreply.github.com>
2020-08-25 17:49:29 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,38 @@
+'use strict';
+var lndPage = document.getElementById('landing-page'),
+    myBaar = document.getElementById('myBaar');
+
+setInterval(function(){
+    var rndNum = Math.floor(Math.random() *5) + 1;
+    lndPage.classList.remove('img1','img2','img3','img4','img5');
+    switch(rndNum)
+        {
+            case 1:
+                lndPage.classList.add('img1');
+                break;
+            case 2:
+                lndPage.classList.add('img2');
+                break;
+            case 3:
+                lndPage.classList.add('img3');
+                break;
+            case 4:
+                lndPage.classList.add('img4');
+                break;
+            case 5:
+                lndPage.classList.add('img5');
+                break;
+        }
+    },
+    3000);
+
+myBaar.onclick = function(){
+    lndPage.classList.toggle('returnMenu');
+    // lndPage.classList.toggle('fixedPo');
+    if(myBaar.getAttribute('class') == 'fas fa-bars'){
+        myBaar.setAttribute('class', 'fas fa-times')
+    }
+    else{
+        myBaar.setAttribute('class', 'fas fa-bars')
+    }
+}
