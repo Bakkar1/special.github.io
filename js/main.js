e88141798b57a46d3b0b81c6686a9357b41e556f
Bakkar1 <68107222+Bakkar1@users.noreply.github.com>
2020-08-26 20:02:35 +0200

    elzero background

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 'use strict';
-var lndPage = document.getElementById('landing-page'),
+var lndPage = document.querySelector('.landing-page'), //n9der njib l elemyn b l class name
     myBaar = document.getElementById('myBaar'),
-    mylis = document.querySelectorAll('li');
-
+    mylis = document.querySelectorAll('.links li');
+/*
+change background img my way using css
 setInterval(function(){
     var rndNum = Math.floor(Math.random() *5) + 1;
     lndPage.classList.remove('img1','img2','img3','img4','img5');
@@ -26,7 +27,21 @@ setInterval(function(){
         }
     },
     3000);
-    
+*/
+
+
+//elzero way change background img only js :
+
+// get array of img
+let imgsArray = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg"]
+setInterval(function(){
+    // get Random number
+    let rndomNum = Math.floor(Math.random() * imgsArray.length);
+    // change background image URL
+    lndPage.style.backgroundImage = "url('../images/"+ imgsArray[rndomNum] +"')";
+}, 10000);
+
+
 function noScroll(){
     window.scroll(0,0);
 }
@@ -46,6 +61,7 @@ myBaar.onclick = function(){
 for(var i = 0; i < mylis.length; i++){
     mylis[i].onclick = function(){
         window.removeEventListener('scroll', noScroll);
-        lndPage.classList.toggle('returnMenu');
+        lndPage.classList.remove('returnMenu');
+        myBaar.setAttribute('class', 'fas fa-bars');
     }
 }
